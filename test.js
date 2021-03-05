e639836ec989777a89ece8c5e9483d3d264ac88b
권유진 <yoojin-kwon@gwon-yujin-ui-MacBookPro.local>
2021-03-05 21:03:43 +0900

    Add: test.js 파일 생성

---
+++ test.js
@@ -0,0 +1,9 @@
+function getEven() {
+    const emptyArray = [];
+
+    for(i=1; i<=50; i++){
+        if(Number.isInteger(i/2) === true){
+            const getArray = emptyArray.push(i);
+        } 
+    } return getArray;
+}
